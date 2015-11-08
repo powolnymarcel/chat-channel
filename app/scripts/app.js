8fb2bdb077708ad414cf23e6e5a417ecb1a02dc1
powolnymarcel <powolnymarcel@gmail.com>
2015-11-08 10:55:53 +0100

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name projet10LeChatApp
+ * @description
+ * # projet10LeChatApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('projet10LeChatApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name projet10LeChatApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the projet10LeChatApp
+ */
+angular.module('projet10LeChatApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
