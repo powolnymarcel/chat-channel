35df3c51fc906593c1f330c6eab14814612ce03b
powolnymarcel <powolnymarcel@gmail.com>
2015-11-09 01:56:28 +0100

    fin

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+var gzippo = require('gzippo');
 var express = require('express');
-var port = process.env.PORT || 3000;
 var app = express();
-app.use(express.static(__dirname + '/app'));
-app.listen(port);
+
+app.use(express.logger('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/app"));
+app.listen(process.env.PORT || 5000);
